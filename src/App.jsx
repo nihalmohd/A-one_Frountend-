349920819c88f9700778ae9eef5e47d9b1094134
nihalmohd <nihalmohd828@gmail.com>
2023-11-28 22:28:18 +0530

    basic settup is over

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Nav from './Components/Nav'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Nav/>
     </>
   )
 }
